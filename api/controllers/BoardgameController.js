d6f254a9ef3a5d091c971b614e1849c3f50e5e8c
kavitachana <kavitachana@gmail.com>
2016-09-08 09:54:44 -0400

    Initial Commit

---
+++ api/controllers/BoardgameController.js
@@ -0,0 +1,11 @@
+/**
+ * BoardgameController
+ *
+ * @description :: Server-side logic for managing boardgames
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
